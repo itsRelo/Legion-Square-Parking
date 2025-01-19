b082b541f8c4c941aa78d07a15899980dc5c6b18
itsRelo <itsRelo97@gmail.com>
2025-01-19 14:20:05 +0100

    Add files via upload

---
+++ itsRelo97_Legion-SquareParking/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean' 
+
+games {'gta5'}  
+
+this_is_a_map 'yes'  
+
+Author 'itsRelo97-7LIFEDEV' 
+
+description 'PARKING-Legion-Square'  
+
+version '1.0'
+dependency '/assetpacks'
